e257e6944c3fb6325c01026307ac4399f0cd7591
Chaitali Merai <meraichaitali75@gmai.com>
2026-01-29 19:21:08 -0500

    fix: Feature: App is functional with localstorage data

--- src/components/TaskManager.jsx
+++ src/components/TaskManager.jsx
@@ -1,18 +1,35 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import TaskForm from './TaskForm';
 import TaskList from './TaskList';
 
 function TaskManager() {
+
+    //LAZY INITIALIZATION: check localstorage 
+    const [tasks, setTasks] = useState(() => {
+        const saved = localStorage.getItem("my-tasks");
+        if(saved){
+            return JSON.parse(saved);
+        }else{
+            return[];
+        }
+    });
+    // 2. THE SIDE EFFECT: Whenever 'tasks' changes, save it!
+    useEffect(() =>{
+        localStorage.setItem("my-tasks", JSON.stringify(tasks));
+    }, [tasks]);
+
     // === 1. STATE lives here now ===
-    const [tasks, setTasks] = useState([
-        { id: 1, text: "Setup Development Environment" },
-        { id: 2, text: "Understand State vs Props" },
-        { id: 3, text: "Complete Task 1" },
-    ]);
+    // const [tasks, setTasks] = useState([
+    //     { id: 1, text: "Setup Development Environment" },
+    //     { id: 2, text: "Understand State vs Props" },
+    //     { id: 3, text: "Complete Task 1" },
+    // ]);
 
     // === 2. LOGIC ===
     const addTask = (text) => {
-        const newTask = { id: Date.now(), text: text };
+        const newTask = { 
+            id: Date.now(), 
+            text: text };
         setTasks([...tasks, newTask]);
     };
 
@@ -25,7 +42,6 @@ function TaskManager() {
         <div className="task-manager">
             <h2>My Daily Tasks</h2>
 
-            {/* Sibling communication is handled here */}
             <TaskForm onAdd={addTask} />
             <TaskList tasks={tasks} onDelete={deleteTask} />
         </div>
